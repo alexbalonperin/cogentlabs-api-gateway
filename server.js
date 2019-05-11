1dce193a614382dee381998a3f9f134cb32ff501
Alexandre Balon-Perin <alexbp13@gmail.com>
2019-05-11 13:55:08 +0900

    rename to avoid conflict

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express()
 var router = require('./src/routers/router')
 var bodyParser = require('body-parser')
 
-const HOST = process.env.HOST || 'localhost'
-const PORT = process.env.PORT || 8080
+const HOST = process.env.API_HOST || 'localhost'
+const PORT = process.env.API_PORT || 8080
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
